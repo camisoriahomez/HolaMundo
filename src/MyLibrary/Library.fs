51290261c4fe9a1834c2b240027ca1198a9fdfe2
camisoriahz <camilasoriahz@gmail.com>
2023-05-15 17:26:20 -0300

    Agrego MathLibrary

--- src/MyLibrary/Library.fs
+++ src/MyLibrary/Library.fs
@@ -3,3 +3,6 @@
 module Say =
     let hello name =
         printfn "Hello %s" name
+
+    let imprimeFloat (x:float) =
+        printfn $"El float es %f{x}"
--- src/MyProject/Program.fs
+++ src/MyProject/Program.fs
@@ -1,2 +1,20 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+// printfn "Hello from F#"
+
+
+// 1. Agrego la biblioteca a este proyecto con
+// dotnet add reference ../../src/MyLibrary/MyLibrary.fsproj
+// estando en el directorio del proyecto
+
+open MyLibrary
+// o
+// importo el modulo con
+// open MyLibrary.Say
+
+[<EntryPoint>]
+
+let main args =
+    Say.hello "Camila"
+    //printfn "Hello World from F#!"
+    MyLibrary.Math.imprimeAdd 2 3
+    0 // return an integer exit code
